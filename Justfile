d5f54a1e6df1838a6863a0ad922a843ff63e7983
Sergei Barbarash <sgt.sgt@gmail.com>
2023-05-17 19:35:16 +0300

    🏸

--- Justfile
+++ Justfile
@@ -6,12 +6,8 @@ server:
     go run server/main.go
 
 # Golang client
-client:
-    go run client/main.go
-
-# test against remote server
-remote *ARGS:
-    go run remote/main.go {{ARGS}}
+client *ARGS:
+    go run client/main.go {{ARGS}}
 
 # glauth server
 glauth:
--- client/main.go
+++ client/main.go
@@ -1,39 +1,62 @@
 package main
 
 import (
-	// "crypto/tls"
-	"github.com/go-ldap/ldap/v3"
+	"fmt"
 	"log"
+	"os"
+
+	"github.com/go-ldap/ldap/v3"
 )
 
 func main() {
-	// The username and password we want to check
-	// username := "foo"
-	// password := "meow"
+	if len(os.Args) != 3 {
+		log.Fatal("need 2 arguments: user and password")
+	}
 
-	l, err := ldap.DialURL("ldap://localhost:10389")
+	username := os.Args[1]
+	password := os.Args[2]
+
+	l, err := ldap.DialURL("ldap://172.16.10.101")
+	// l, err := ldap.DialURL("ldap://127.0.0.1:10389")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer l.Close()
 
-	// Reconnect with TLS
-	// err = l.StartTLS(&tls.Config{InsecureSkipVerify: true})
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	baseDN := "cn=users,cn=accounts,dc=ipa,dc=abc-p,dc=com"
 
-	// First bind with a read only user
-	// err = l.Bind(username, password)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	l.err = l.UnauthenticatedBind("readonly")
+	ldapUsername := fmt.Sprintf("uid=%s,%s", username, baseDN)
+	err = l.Bind(ldapUsername, password)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println("user authenticated")
+	sr, err := l.Search(&ldap.SearchRequest{
+		BaseDN: baseDN,
+		Filter: "(uid=sgt)",
+		// Filter:     fmt.Sprintf("(&(uid=%s))", username),
+		Attributes: []string{"telephoneNumber"},
+		Scope:      ldap.ScopeWholeSubtree,
+		TimeLimit:  8,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(sr.Entries) != 1 {
+		log.Fatal("no single entry found")
+	}
+
+	entry := sr.Entries[0]
+	if len(entry.Attributes) != 1 || entry.Attributes[0].Name != "telephoneNumber" {
+		log.Fatal("no telephone number entries")
+	}
+
+	phoneNumbers := entry.Attributes[0].Values
+	if len(phoneNumbers) == 0 {
+		log.Fatal("no telephone numbers")
+	}
+
+	log.Println(phoneNumbers[0])
 
 }
